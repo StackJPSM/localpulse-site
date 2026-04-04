671b39d924c4c10f7b2d32397bb31899454f5c94
StackJPSM <jetpackspacemen@gmail.com>
2026-04-04 09:33:35 -0500

    Create generate-posts.js

---
+++ scripts/generate-posts.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const postsDir = path.join(__dirname, "../content/products");
+const outputFile = path.join(__dirname, "../posts.json");
+
+let posts = [];
+
+fs.readdirSync(postsDir).forEach(file => {
+  if (file.endsWith(".md")) {
+    const content = fs.readFileSync(path.join(postsDir, file), "utf-8");
+
+    // Simple parsing (basic frontmatter)
+    const titleMatch = content.match(/title:\s*(.*)/);
+    const imageMatch = content.match(/image:\s*(.*)/);
+    const descMatch = content.match(/description:\s*(.*)/);
+
+    posts.push({
+      title: titleMatch ? titleMatch[1] : "No title",
+      image: imageMatch ? imageMatch[1] : "",
+      description: descMatch ? descMatch[1] : ""
+    });
+  }
+});
+
+fs.writeFileSync(outputFile, JSON.stringify(posts, null, 2));
+
+console.log("posts.json generated!");
